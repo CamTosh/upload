938191e4e925c54344defb28cfd08cf623b9b2c4
qeremy <qeremy@gmail.com>
2013-01-26 01:45:28 +0200

    ...

--- test.php
+++ test.php
@@ -2,18 +2,18 @@
 
 
 <form method="post" action="" enctype="multipart/form-data">
-	File: <input type="file" name="file">
-	<input type="submit" name="submit" value="Send">
+    File: <input type="file" name="file">
+    <input type="submit" name="submit" value="Send">
 </form>
 
 <?
 if (isset($_POST['submit'])) {
-	$up = new Upload($_FILES['file']);
-	if (!$up->isError()) {
-		$up->load();
-	} else {
-		print $up->getErrors(true);
-	}
-	pre($up);
+    $up = new Upload($_FILES['file']);
+    if (!$up->isError()) {
+        $up->load();
+    } else {
+        print $up->getErrors(true);
+    }
+    pre($up);
 }
 ?>
